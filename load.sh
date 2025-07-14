2fc29796cde4f1ee8083d9bdb7acabcafce25b49
Andrew Savory <savs@andrewsavory.com>
2025-07-14 08:12:44 -0700

    add k8s deploy config

--- load.sh
+++ load.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Bash script to repeatedly hit http://localhost:8080/ with a random pause between 0 and 2 seconds
 
+ADDRESS=10.0.0.31
+PORT=30080
+
 count=0
 while true; do
-  response=$(curl -s http://localhost:8080/)
+  response=$(curl -s http://$ADDRESS:$PORT/)
   count=$((count+1))
   echo "Response: $response"
   echo "Total requests made: $count"
   # Sleep for a random duration between 0 and 2 seconds
-  sleep_time=$(awk -v min=0 -v max=2 'BEGIN{srand(); print min+rand()*(max-min)}')
+  sleep_time=$(awk -v min=0 -v max=5 'BEGIN{srand(); print min+rand()*(max-min)}')
   sleep $sleep_time
 done
